9b1adad47fd3009a9f1483b263888d3e38cbed5f
liza9797 <edlazareva@gmail.com>
2025-02-25 09:56:11 +0300

    Rename models

--- src/models/SiameseNNSPP.py
+++ src/models/SiameseNNSPP.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class EncoderSPP(nn.Module):
+class SiameseNNSPP(nn.Module):
     def __init__(self):
         super().__init__()
 
